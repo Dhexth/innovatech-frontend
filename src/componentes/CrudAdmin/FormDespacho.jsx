df6a39c5da7faf67007e68a2f4cd1caaae894d95
Ariel Ortiz <ari.ortiz@duocuc.cl>
2026-07-01 03:02:55 -0400

    fix: reemplazar última IP hardcodeada en FormDespacho

--- src/componentes/CrudAdmin/FormDespacho.jsx
+++ src/componentes/CrudAdmin/FormDespacho.jsx
@@ -34,7 +34,7 @@ export const FormDespacho = ({ venta, onClose }) => {
       }
         }
       );
-      await axios.post("http://192.168.320/api/v1/despachos", jsonData, {
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/v1/despachos`, jsonData, {
         headers:{
           'Content-Type': 'application/json',
           'Accept': 'application/json'
